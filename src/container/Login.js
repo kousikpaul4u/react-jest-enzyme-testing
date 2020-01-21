d742135ca1164172aa8c10d5d3916c3f612e7b2a
Koushik Pal <koushik.pal@ascendcorp.copm>
2020-01-21 12:51:34 +0700

    State instance unit test added

--- src/container/Login.js
+++ src/container/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Button from 'components/button/button';
@@ -18,22 +18,42 @@ const mapStateToProps = state => {
 };
 
 
-function Login(props) {
+class Login extends Component {
 
-    function onClick() {
-        props.fetchPosts();
+    constructor() {
+        super();
+        this.state = {
+            hideButton: false
+        }
     }
 
-    return (
-        <div>
-            <Button id="login-button" text="Submit" type="button" onClick={onClick} />
-            {
-                props.data && props.data.map((post) =>
-                    <ListItems title={post.title} desc={post.body} />
-                )
-            }
-        </div>
-    )
+    onClick = () => {
+        this.hideSubmitButton();
+        this.props.fetchPosts();
+    }
+
+    hideSubmitButton = () => {
+        this.setState({
+            hideButton: !this.state.hideButton
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.hideButton ?
+                        null
+                        : <Button data-test="submit-button" id="login-button" text="Submit" type="button" onClick={this.onClick} />
+                }
+                {
+                    this.props.data && this.props.data.map((post) =>
+                        <ListItems key={post.title} data-test="list-items" title={post.title} desc={post.body} />
+                    )
+                }
+            </div>
+        )
+    }
 
 }
 
--- src/container/Login.test.js
+++ src/container/Login.test.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Login from './Login';
-import { findByTestAttr, testPropTypes } from 'utils/TestUtils';
-
-const setUp = (props = {}) => {
-    return shallow(<Login {...props} />);
-}
+import { findByTestAttr, testPropTypes, testStore } from 'utils/TestUtils';
+import { exportAllDeclaration } from '@babel/types';
 
 describe('<Login /> component', () => {
 
@@ -23,5 +20,53 @@ describe('<Login /> component', () => {
         });
 
     });
+
+    describe('Renders', () => {
+
+        const setUp = (initialState = {}) => {
+            const store = testStore(initialState);
+            const wrapper = shallow(<Login store={store} />).childAt(0).dive();
+            return wrapper;
+        };
+
+        let component;
+        let mockFunc;
+        beforeEach(() => {
+            mockFunc = jest.fn();
+            component = setUp({
+                common: {
+                    data: [{
+                        title: "Koushik Pal",
+                        body: "Test",
+                    }]
+                }
+            });
+        })
+
+        it('Should render', () => {
+            const wrapper = findByTestAttr(component, 'submit-button');
+            expect(wrapper.length).toBe(1);
+        })
+
+        it('Should update hideButton state', () => {
+            const classInstance = component.instance();
+            classInstance.onClick();
+            const newState = classInstance.state.hideButton;
+            expect(newState).toBe(true);
+        });
+
+        // it('Should render', () => {
+        //     const wrapper = findByTestAttr(component, 'list-items');
+        //     expect(wrapper.length).toBe(1);
+        // })
+
+        // it('Should emit callback on click event', () => {
+        //     const button = findByTestAttr(component, 'submit-button');
+        //     button.simulate('click');
+        //     const callback = mockFunc.mock.calls.length;
+        //     expect(callback).toBe(1);
+        // });
+
+    })
 
 });
